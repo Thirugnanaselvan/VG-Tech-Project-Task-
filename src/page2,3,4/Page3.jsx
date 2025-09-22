be8a76f409baff5130a1181553638d9dbed8596b
Thirugnanaselvan G P <thirutamilan07052004@gmail.com>
2025-09-22 10:55:15 +0530

    Update Page3.jsx

--- src/page2,3,4/Page3.jsx
+++ src/page2,3,4/Page3.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import '../App.css'
 import Formcard from '../page3/Formcard'
 import scroll from '../assets/scroll.png'
-import Page4 from './page4.jsx'
+import Page4 from './Page4.jsx'
 function Page3() {
   return (
     <div>
